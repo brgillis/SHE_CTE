e859cb4268ee7addcdccdd365144d32bc6ad29f3
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-09-01 16:10:47 +0100

    Adding inv_var_stack unit test

--- SHE_CTE_ShearEstimation/tests/python/test_estimate_shear.py
+++ SHE_CTE_ShearEstimation/tests/python/test_estimate_shear.py
@@ -37,24 +37,62 @@ class TestCase:
 
     def test_get_resampled_image(self):
         
-        ss_factor = 5
+        for ss_scale in (0.2,0.4):
+            for rb_scale in (0.8,1.2):
         
-        # Make a mock subsampled image
-        ss_data = np.zeros((3*ss_factor,3*ss_factor))
-        for i in range(3):
-            for j in range(3):
-                ss_data[ ss_factor*i:ss_factor*i+ss_factor, ss_factor*j:ss_factor*j+ss_factor ] += i + 3*j
+                ss_factor = int(round(rb_scale/ss_scale))
                 
-        ss_data /= ss_data.sum()
+                # Make a mock subsampled image
+                ss_data = np.zeros((3*ss_factor,3*ss_factor))
+                for i in range(3):
+                    for j in range(3):
+                        ss_data[ ss_factor*i:ss_factor*i+ss_factor, ss_factor*j:ss_factor*j+ss_factor ] += i + 3*j
+                        
+                ss_data /= ss_data.sum()
+                        
+                ss_image = SHEImage(ss_data)
+                ss_image.header[scale_label] = 1./ss_factor
                 
-        ss_image = SHEImage(ss_data)
-        ss_image.header[scale_label] = 1./ss_factor
+                # Try rebinning it
+                rb_image = get_resampled_image(ss_image,1.)
+                rb_data = rb_image.data
+                rb_data /= rb_data.sum()
+                
+                # Check the sections are close
+                for i in range(3):
+                    for j in range(3):
+                        assert np.isclose( ss_data[ ss_factor*i:ss_factor*i+ss_factor, ss_factor*j:ss_factor*j+ss_factor ].sum(),
+                                           rb_data[ i:i+1, j:j+1 ].sum() )
         
-        rb_image = get_resampled_image(ss_image,1.)
+    def test_inv_var_stack(self):
         
-        rb_data = rb_image.data
+        a = np.array([1.0, 2.0, 2.0])
+        a_err = np.array([1.0, 0.5, 2.0])
         
-        rb_data /= rb_data.sum()
+        # Test expected result
         
+        a_m, a_m_err = inv_var_stack(a,a_err)
         
-        assert True
+        assert np.isclose( a_m, 1.8095238095238095)
+        assert np.isclose( a_m_err, 0.19047619047619047)
+        
+        # Test addition
+        
+        ap1_m, ap1_m_err = inv_var_stack(a+1,a_err)
+        
+        assert np.isclose( ap1_m, a_m+1 )
+        assert np.isclose( ap1_m_err, a_m_err )
+        
+        # Test multiplication
+        
+        at2_m, at2_m_err = inv_var_stack(a*2,a_err)
+        
+        assert np.isclose( at2_m, a_m*2 )
+        assert np.isclose( at2_m_err, a_m_err )
+        
+        # Test changing the error
+        
+        aet2_m, aet2_m_err = inv_var_stack(a,a_err*2)
+        
+        assert np.isclose( aet2_m, a_m )
+        assert np.isclose( aet2_m_err, a_m_err*2 )
