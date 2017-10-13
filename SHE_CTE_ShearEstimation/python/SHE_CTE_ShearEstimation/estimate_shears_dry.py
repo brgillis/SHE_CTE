665e3ebc8d51e2e24291fe28d9e43304d1d73a57
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-10-13 18:20:26 +0100

    Fixing read of she images

--- SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/estimate_shears_dry.py
+++ SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/estimate_shears_dry.py
@@ -33,7 +33,8 @@ from SHE_PPT.file_io import (read_listfile, write_listfile,
                              get_allowed_filename, append_hdu)
 from SHE_PPT.galaxy_population_table_format import tf as gptf
 from SHE_PPT.psf_table_format import tf as psft
-from SHE_PPT.she_image import she_image
+from SHE_PPT.she_image import SHEImage
+from SHE_PPT.she_stack import SHEStack
 from SHE_PPT import shear_estimates_product as sep
 from SHE_PPT.shear_estimates_table_format import tf as setf 
 from SHE_PPT.table_utility import is_in_format
@@ -50,12 +51,56 @@ def estimate_shears_from_args(args):
     # Load in the files in turn to make sure there aren't any issues with them.
     
     # Data images - Read in as SHEStack object
+#     she_stack = SHEStack.read(data_images,
+#                               data_ext = ppt_mv.sci_tag,
+#                               mask_ext = ppt_mv.mask_tag,
+#                               noisemap_ext = ppt_mv.noisemap_tag)
+
+    sci_hdus = []
+    noisemap_hdus = []
+    mask_hdus = []
     
-    data_images = read_listfile(args.data_images)
-    she_stack = SHEStack.read(data_images,
-                              data_ext = ppt_mv.sci_tag,
-                              mask_ext = ppt_mv.mask_tag,
-                              noisemap_ext = ppt_mv.noisemap_tag)
+    she_images = []
+    
+    for i, filename in enumerate(data_images):
+        
+        data_image_hdulist = fits.open(filename, mode="readonly", memmap=True)
+        num_detectors = len(data_image_hdulist) // 3
+        
+        sci_hdus.append([])
+        noisemap_hdus.append([])
+        mask_hdus.append([])
+        she_images.append([])
+        
+        for j in range(num_detectors):
+            
+            sci_extname = str(j) + "." + ppt_mv.sci_tag
+            sci_index = find_extension(data_image_hdulist, sci_extname)
+            
+            sci_hdus[i].append( data_image_hdulist[sci_index] )
+            
+            noisemap_extname = str(j) + "." + ppt_mv.noisemap_tag
+            noisemap_index = find_extension(data_image_hdulist, noisemap_extname)
+            
+            noisemap_hdus[i].append( data_image_hdulist[noisemap_index] )
+            
+            mask_extname = str(j) + "." + ppt_mv.mask_tag
+            mask_index = find_extension(data_image_hdulist, mask_extname)
+            
+            mask_hdus[i].append( data_image_hdulist[mask_index] )
+            
+            she_images[i].append(SHEImage(data=sci_hdus[i][j].data,
+                                          noisemap=noisemap_hdus[i][j].data,
+                                          mask=mask_hdus[i][j].data,
+                                          header=sci_hdus[i][j].header))
+            
+    num_exposures = len(data_images)
+    she_stacks = []
+    for j in range(num_detectors):
+        detector_images = []
+        for i in range(num_exposures):
+            detector_images.append(she_images[i][j])
+        she_stacks.append(detector_images)
     
     # Detections tables
     
