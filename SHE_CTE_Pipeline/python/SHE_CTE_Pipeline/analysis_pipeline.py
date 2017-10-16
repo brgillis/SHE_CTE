fea12d427c68085809897a1b86e0e2f3aef3e5b3
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-10-16 16:18:40 +0100

    Adding needed import

--- SHE_CTE_Pipeline/python/SHE_CTE_Pipeline/analysis_pipeline.py
+++ SHE_CTE_Pipeline/python/SHE_CTE_Pipeline/analysis_pipeline.py
@@ -20,7 +20,7 @@
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 """
 
-from SHE_CTE_Pipeline.package_definition import she_estimate_shear, she_validate_shear
+from SHE_CTE_Pipeline.package_definition import she_fit_psf, she_estimate_shear, she_validate_shear
 
 @pipeline(outputs=('validated_shear_estimates_table'))
 def shear_analysis_pipeline( data_images,
