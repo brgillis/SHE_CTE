101d6c29f88439cb053283fbc39fca504911f0b4
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-10-21 00:04:47 +0200

    Updating shear estimation code for new interface

--- SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/galsim_estimate_shear.py
+++ SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/galsim_estimate_shear.py
@@ -21,20 +21,20 @@
 """
 
 from math import sqrt
-import numpy as np
+
 import galsim
 
-from SHE_GST_IceBRGpy.logging import getLogger
-
+from SHE_CTE_ShearEstimation import magic_values as mv
 from SHE_GST_GalaxyImageGeneration.noise import get_var_ADU_per_pixel
 from SHE_GST_GalaxyImageGeneration.unweighted_moments import get_g_from_e
-
+from SHE_GST_IceBRGpy.logging import getLogger
 from SHE_PPT.detections_table_format import tf as detf
-from SHE_PPT.shear_estimates_table_format import initialise_shear_estimates_table, tf as setf
-from SHE_PPT.she_image import SHEImage
 from SHE_PPT.magic_values import scale_label, stamp_size_label
+from SHE_PPT.psf_table_format import tf as pstf
+from SHE_PPT.she_image import SHEImage
+from SHE_PPT.shear_estimates_table_format import initialise_shear_estimates_table, tf as setf
+import numpy as np
 
-from SHE_CTE_ShearEstimation import magic_values as mv
 
 class ShearEstimate(object):
     def __init__(self, g1, g2, gerr=None):
@@ -164,15 +164,26 @@ def inv_var_stack( a, a_err ):
         
     logger.debug("Exiting inv_var_stack")
 
-def GS_estimate_shear( data_stack, method, shape_noise_var ):
+def GS_estimate_shear( data_stack, method ):
 
     logger = getLogger(mv.logger_name)
     logger.debug("Entering estimate_shear_gs")
     
     # Get lists of exposures, PSF images, and detection tables
-    data_images = data_stack.get_data_images()
-    psf_images = data_stack.get_psf_images()
-    detection_tables = data_stack.get_detection_tables_images()
+    disk_psf_imagesdata_images = []
+    detection_tables = []
+    bulge_psf_images = []
+    disk_psf_images = []
+    psf_tables = []
+    
+    num_exposures = len(data_stack.exposures)
+    
+    for i in range(num_exposures):
+        data_images.append(data_stack.exposures[i].science_image)
+        detection_tables.append(data_stack.exposures[i].detections_table)
+        bulge_psf_images.append(data_stack.exposures[i].bpsf_image)
+        disk_psf_images.append(data_stack.exposures[i].dpsf_image)
+        psf_tables.append(data_stack.exposures[i].psf_table)
     
     # Calculate the sky variances
     sky_vars = []
@@ -183,19 +194,18 @@ def GS_estimate_shear( data_stack, method, shape_noise_var ):
                                                 pixel_scale=data_image.scale,
                                                 gain=detections_table[detf.m.gain]))
     
-    num_tables = len(detections_tables)
-    
     # Get all unique IDs
     IDs = None
-    for table_index in range(num_tables):
+    for table_index in range(num_exposures):
         if IDs is None:
             IDs = set(detections_tables[table_index][detf.ID])
         else:
             IDs = set.union(IDs,detections_tables[table_index][detf.ID])
             
     # Set the ID as an index for each table
-    for detection_table in detection_tables:
-        detection_table.add_index(detf.ID)
+    for table_index in range(num_exposures):
+        detection_tables[table_index].add_index(detf.ID)
+        psf_tables[table_index].add_index(pstf.ID)
     
     shear_estimates_table = initialise_shear_estimates_table(detections_tables[0],
                                                              optional_columns=[setf.e1_err,setf.e2_err])
@@ -206,22 +216,26 @@ def GS_estimate_shear( data_stack, method, shape_noise_var ):
         g2s = []
         gerrs = []
         
-        for detections_table in detections_tables:
+        for table_index in range(num_exposures):
             
             try:
                 
-                # Get the row for this ID
-                row = detections_table.loc[ID]
+                # Get the rows for this ID
+                g_row = detections_tables[table_index].loc[ID]
+                p_row = psf_tables[table_index].loc[ID]
             
                 # Get galaxy and PSF stamps
-                gal_stamp = data_image.extract_stamp(row[detf.gal_x],
-                                                     row[detf.gal_y],
-                                                     data_image.header[stamp_size_label])
-                psf_stamp = psf_image.extract_stamp(row[detf.psf_x],
-                                                    row[detf.psf_y],
-                                                    psf_image.header[stamp_size_label])
+                gal_stamp = data_images[table_index].extract_stamp(g_row[detf.gal_x],
+                                                                   g_row[detf.gal_y],
+                                                                   data_images[table_index].header[stamp_size_label])
+                bulge_psf_stamp = bulge_psf_images[table_index].extract_stamp(p_row[pstf.psf_x],
+                                                                              p_row[pstf.psf_y],
+                                                                              bulge_psf_images[table_index].header[stamp_size_label])
+                disk_psf_stamp = disk_psf_images[table_index].extract_stamp(p_row[pstf.psf_x],
+                                                                              p_row[pstf.psf_y],
+                                                                              disk_psf_images[table_index].header[stamp_size_label])
         
-                shear_estimate = get_shear_estimate(gal_stamp, psf_stamp, sky_vars[table_index], method, shape_noise_var)
+                shear_estimate = get_shear_estimate(gal_stamp, bulge_psf_stamp, sky_vars[table_index], method)
                 
                 g1s.append(shear_estimate.g1)
                 g2s.append(shear_estimate.g2)
