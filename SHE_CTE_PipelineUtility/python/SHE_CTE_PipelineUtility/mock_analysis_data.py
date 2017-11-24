52e69115632359fe64fe707b82138a8e11ff3d2d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-11-24 19:17:42 +0000

    Attempted fix

--- SHE_CTE_PipelineUtility/python/SHE_CTE_PipelineUtility/mock_analysis_data.py
+++ SHE_CTE_PipelineUtility/python/SHE_CTE_PipelineUtility/mock_analysis_data.py
@@ -267,7 +267,7 @@ def make_mock_analysis_data(args, dry_run=False):
     
     logger.info("Generating mock dry galaxy population priors table...")
     
-    filename = get_allowed_filename("GALPOP", "0", extension=".bin")
+    filename = get_allowed_filename("GALPOP", "0", extension=".fits")
     galaxy_population_prod = galaxy_population_product.create_galaxy_population_product(filename=filename)
     
     write_pickled_product(galaxy_population_prod, args.galaxy_population_priors_table)
