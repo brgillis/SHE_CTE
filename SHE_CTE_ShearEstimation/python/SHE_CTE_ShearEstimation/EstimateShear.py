7095143f8fc7eeefb37b2021d1b3f6bfe6acdce3
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-04 15:18:32 +0200

    Use default None here too

--- SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/EstimateShear.py
+++ SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/EstimateShear.py
@@ -34,82 +34,82 @@ def defineSpecificProgramOptions():
         An ArgumentParser.
     """
 
-    logger = getLogger( mv.logger_name )
+    logger = getLogger(mv.logger_name)
 
-    logger.debug( '#' )
-    logger.debug( '# Entering SHE_CTE_EstimateShear defineSpecificProgramOptions()' )
-    logger.debug( '#' )
+    logger.debug('#')
+    logger.debug('# Entering SHE_CTE_EstimateShear defineSpecificProgramOptions()')
+    logger.debug('#')
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument( '--profile', action = 'store_true',
-                        help = 'Store profiling data for execution.' )
-    parser.add_argument( '--dry_run', action = 'store_true',
-                        help = 'Dry run (no data processed).' )
-    parser.add_argument( '--debug', action = 'store_true',
-                        help = 'Enables debug mode - only process first 1000 galaxies.' )
+    parser.add_argument('--profile', action = 'store_true',
+                        help = 'Store profiling data for execution.')
+    parser.add_argument('--dry_run', action = 'store_true',
+                        help = 'Dry run (no data processed).')
+    parser.add_argument('--debug', action = 'store_true',
+                        help = 'Enables debug mode - only process first 1000 galaxies.')
 
     # Required input arguments
 
-    parser.add_argument( '--data_images', type = str,
-                        help = '.json listfile containing filenames of data image products.' )
+    parser.add_argument('--data_images', type = str,
+                        help = '.json listfile containing filenames of data image products.')
 
-    parser.add_argument( '--stacked_image', type = str,
-                        help = 'Filename of the stacked image product' )
+    parser.add_argument('--stacked_image', type = str,
+                        help = 'Filename of the stacked image product')
 
-    parser.add_argument( '--psf_images_and_tables', type = str,
-                        help = '.json listfile containing filenames of psf image products.' )
+    parser.add_argument('--psf_images_and_tables', type = str,
+                        help = '.json listfile containing filenames of psf image products.')
 
-    parser.add_argument( '--segmentation_images', type = str,
-                        help = '.json listfile containing filenames of segmentation map images.' )
+    parser.add_argument('--segmentation_images', type = str,
+                        help = '.json listfile containing filenames of segmentation map images.')
 
-    parser.add_argument( '--stacked_segmentation_image', type = str,
-                        help = 'Filename of the stacked segmentation map image.' )
+    parser.add_argument('--stacked_segmentation_image', type = str,
+                        help = 'Filename of the stacked segmentation map image.')
 
-    parser.add_argument( '--detections_tables', type = str,
-                        help = '.json listfile containing filenames of detections table products.' )
+    parser.add_argument('--detections_tables', type = str,
+                        help = '.json listfile containing filenames of detections table products.')
 
-    parser.add_argument( '--bfd_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
-                        help = 'Data product for BFD training data.' )
+    parser.add_argument('--bfd_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
+                        help = 'Data product for BFD training data.')
 
-    parser.add_argument( '--ksb_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
-                        help = 'Data product for KSB training data.' )
+    parser.add_argument('--ksb_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
+                        help = 'Data product for KSB training data.')
 
-    parser.add_argument( '--lensmc_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
-                        help = 'Data product for LensMC training data.' )
+    parser.add_argument('--lensmc_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
+                        help = 'Data product for LensMC training data.')
 
-    parser.add_argument( '--momentsml_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
-                        help = 'Data product for MomentsML training data.' )
+    parser.add_argument('--momentsml_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
+                        help = 'Data product for MomentsML training data.')
 
-    parser.add_argument( '--regauss_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
-                        help = 'Data product for REGAUSS training data.' )
+    parser.add_argument('--regauss_training_data', type = str, default = None,  # Use default in case we don't use it for SC4
+                        help = 'Data product for REGAUSS training data.')
 
-    parser.add_argument( '--galaxy_population_priors_table', type = str,
-                        help = '.json listfile containing filenames of detections table products.' )
+    parser.add_argument('--galaxy_population_priors_table', type = str, default = None,  # Use default in case we don't use it for SC4
+                        help = '.json listfile containing filenames of detections table products.')
 
-    parser.add_argument( '--calibration_parameters_product', type = str, default = None,  # Use default in case we don't use it for SC4
-                        help = 'Filename of calibration parameters product (XML data product).' )
+    parser.add_argument('--calibration_parameters_product', type = str, default = None,  # Use default in case we don't use it for SC4
+                        help = 'Filename of calibration parameters product (XML data product).')
 
     # Optional input arguments (cannot be used in pipeline)
 
-    parser.add_argument( '--methods', type = str, nargs = '*', default = [],
-                        help = 'Which shear estimation methods to apply. If not specified, all will be run.' )
+    parser.add_argument('--methods', type = str, nargs = '*', default = [],
+                        help = 'Which shear estimation methods to apply. If not specified, all will be run.')
 
     # Output arguments
 
-    parser.add_argument( '--shear_estimates_product', type = str,
-                        help = 'XML data product to contain file links to the shear estimates tables.' )
+    parser.add_argument('--shear_estimates_product', type = str,
+                        help = 'XML data product to contain file links to the shear estimates tables.')
 
     # Arguments needed by the pipeline runner
-    parser.add_argument( '--workdir', type = str, default = "." )
-    parser.add_argument( '--logdir', type = str, default = "." )
+    parser.add_argument('--workdir', type = str, default = ".")
+    parser.add_argument('--logdir', type = str, default = ".")
 
-    logger.debug( '# Exiting SHE_CTE_EstimateShear defineSpecificProgramOptions()' )
+    logger.debug('# Exiting SHE_CTE_EstimateShear defineSpecificProgramOptions()')
 
     return parser
 
 
-def mainMethod( args ):
+def mainMethod(args):
     """
     @brief
         The "main" method for this program, to estimate shears.
@@ -119,25 +119,25 @@ def mainMethod( args ):
         similar to a main (and it is why it is called mainMethod()).
     """
 
-    logger = getLogger( mv.logger_name )
+    logger = getLogger(mv.logger_name)
 
-    logger.debug( '#' )
-    logger.debug( '# Entering SHE_CTE_EstimateShear mainMethod()' )
-    logger.debug( '#' )
+    logger.debug('#')
+    logger.debug('# Entering SHE_CTE_EstimateShear mainMethod()')
+    logger.debug('#')
 
     dry_run = args.dry_run or force_dry_run
 
     if args.profile:
         import cProfile
-        cProfile.runctx( "estimate_shears_from_args(args)", {},
+        cProfile.runctx("estimate_shears_from_args(args)", {},
                         {"estimate_shears_from_args":estimate_shears_from_args,
                          "args":args,
                          "dry_run":dry_run},
-                        filename = "estimate_shears.prof" )
+                        filename = "estimate_shears.prof")
     else:
-        estimate_shears_from_args( args, dry_run )
+        estimate_shears_from_args(args, dry_run)
 
-    logger.debug( '# Exiting SHE_CTE_EstimateShear mainMethod()' )
+    logger.debug('# Exiting SHE_CTE_EstimateShear mainMethod()')
 
     return
 
@@ -151,7 +151,7 @@ def main():
 
     args = parser.parse_args()
 
-    mainMethod( args )
+    mainMethod(args)
 
     return
 
