c01804078ec755e77c1fed7e883202cb6a0fa7f7
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-19 13:06:27 +0200

    Initing

--- SHE_CTE_PSFFitting/python/SHE_CTE_PSFFitting/fit_psfs.py
+++ SHE_CTE_PSFFitting/python/SHE_CTE_PSFFitting/fit_psfs.py
@@ -37,6 +37,7 @@ from SHE_PPT.table_formats.detections import tf as detf
 from SHE_PPT.table_utility import is_in_format, table_to_hdu
 import numpy as np
 
+products.psf_field_params.init()
 
 test_mode = True
 
@@ -105,7 +106,7 @@ def fit_psfs(args, dry_run = False):
 
     for i in range(len(frame_stack.exposures)):
 
-        field_param_filename = get_allowed_filename("PSF_FieldParam", str(i))
+        field_param_filename = get_allowed_filename("PSF_FieldParam", str(i), extension=".xml")
         field_param_filenames.append(field_param_filename)
 
         field_param_product = products.psf_field_params.create_dpd_she_psf_field_params()
