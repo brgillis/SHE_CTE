6949d2fdbeac956d0e478d04e4134902c2a64bc4
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-21 15:45:53 +0100

    Filling in first pass at top-level measure_statistics function

--- SHE_CTE_BiasMeasurement/python/SHE_CTE_BiasMeasurement/magic_values.py
+++ SHE_CTE_BiasMeasurement/python/SHE_CTE_BiasMeasurement/magic_values.py
@@ -5,28 +5,23 @@
     Magic values for measuring bias of shear estimates.
 """
 
-# Copyright (C) 2012-2020 Euclid Science Ground Segment      
-#        
-# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General    
-# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)    
-# any later version.    
-#        
-# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied    
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more    
-# details.    
-#        
-# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to    
-# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-
-from SHE_PPT.shear_estimates_format import *
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
 
 logger_name = "SHE_CTE_MeasureBias"
 
-fits_table_sim_g1_label = "SIM_G1"
-fits_table_sim_g2_label = "SIM_G2"
-
-default_output_filename = "shear_biases.fits"
-default_output_format = "fits"
+estimation_methods = ["KSB", "REGAUSS", "LensMC", "MomentsML", "BFD"]
 
 var_e = {"m2": 0.30241731263684996,
          "m1": 0.27099491059570091,
