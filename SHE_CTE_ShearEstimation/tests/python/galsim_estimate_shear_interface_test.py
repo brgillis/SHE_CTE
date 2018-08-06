bc61a108ef73054e792c2d935ecf533b8d9c276f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-08-06 18:01:00 +0200

    Adding interface test for KSB

---
+++ SHE_CTE_ShearEstimation/tests/python/galsim_estimate_shear_interface_test.py
@@ -0,0 +1,66 @@
+""" @file galsim_estimate_shear_interface_test.py
+
+    Created 6 August 2018
+
+    Unit tests for the control shear estimation methods.
+"""
+
+__updated__ = "2018-08-06"
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import pytest
+
+from SHE_CTE_ShearEstimation.galsim_estimate_shear import (KSB_estimate_shear, REGAUSS_estimate_shear)
+from SHE_PPT.file_io import read_pickled_product, find_file
+from SHE_PPT.table_formats.shear_estimates import tf as setf
+import numpy as np
+
+she_frame_location = "AUX/SHE_PPT/test_she_frame_stack_simple.bin"
+ksb_training_location = "AUX/SHE_PPT/test_KSB_training_data.bin"
+regauss_training_location = "AUX/SHE_PPT/test_REGAUSS_training_data.bin"
+
+class TestCase:
+    """
+
+
+    """
+
+    @pytest.fixture(autouse=True)
+    def setup(self, tmpdir):
+        self.workdir = tmpdir.strpath
+        self.logdir = join(tmpdir.strpath, "logs")
+
+        return
+
+    def test_ksb(self):
+        """Test that the interface for the KSB method works properly.
+        """
+        
+        # Read in the test data
+        she_frame = read_pickled_product(find_file(she_frame_location))
+        ksb_training_data= read_pickled_product(find_file(ksb_training_location))
+        
+        ksb_cat = KSB_estimate_shear(she_frame,
+                                     training_data=ksb_training_data,
+                                     workdir=self.workdir)
+        
+        # Check that we have valid data
+        for row in ksb_cat:
+            for colname in (setf.g1, setf.g2):
+                g = row[colname]
+                if not (g>-1 and g<1):
+                    raise Exception("Bad value for " + colname + ": " + str(g))
+                
+        return
