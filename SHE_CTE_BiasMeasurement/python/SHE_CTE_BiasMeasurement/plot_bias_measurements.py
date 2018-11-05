6a7b460d6294bea43f8ec47b0c1505916376c01c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-11-05 15:12:49 +0000

    Initial setup for clarifying norming

--- SHE_CTE_BiasMeasurement/python/SHE_CTE_BiasMeasurement/plot_bias_measurements.py
+++ SHE_CTE_BiasMeasurement/python/SHE_CTE_BiasMeasurement/plot_bias_measurements.py
@@ -175,7 +175,8 @@ def plot_bias_measurements_from_args(args):
                     ax = fig.add_subplot(1, 1, 1)
                     ax.set_xlabel(testing_data_labels[testing_data_key], fontsize=fontsize)
                     if calibration_label == "_normed":
-                        ax.set_ylabel(r"$\Delta " + measurement_key + "$", fontsize=fontsize)
+                        ax.set_ylabel(r"$\Delta " + measurement_key +
+                                      "$ (relative to X=%0.00f)" % x_values[testing_data_key][2], fontsize=fontsize)
                     else:
                         ax.set_ylabel("$" + measurement_key.replace("_err", r"_{\rm err}") + "$", fontsize=fontsize)
 
