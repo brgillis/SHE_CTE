43f2a061699eb964a6008147e94fffce6faaac0a
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-03-29 14:47:47 +0000

    Updating to merge tables, not ID list

--- SHE_CTE_PipelineUtility/python/SHE_CTE_PipelineUtility/ShearEstimatesMerge.py
+++ SHE_CTE_PipelineUtility/python/SHE_CTE_PipelineUtility/ShearEstimatesMerge.py
@@ -5,7 +5,7 @@
     Merge point executable for merging up batches into objects.
 """
 
-__updated__ = "2019-03-19"
+__updated__ = "2019-03-29"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -23,14 +23,15 @@ __updated__ = "2019-03-19"
 import argparse
 import os
 
+from SHE_PPT import products
 from SHE_PPT.file_io import (read_listfile, write_listfile,
                              read_xml_product, write_xml_product,
                              get_allowed_filename)
 from SHE_PPT.logging import getLogger
-from SHE_PPT.products import object_id_list, shear_estimates
 from SHE_PPT.table_formats.shear_estimates import tf as setf
 from SHE_PPT.table_utility import is_in_format
 from SHE_PPT.utility import get_arguments_string
+from astropy import table
 from astropy.table import Table
 
 
@@ -78,49 +79,114 @@ def object_id_merge_from_args(args):
 
     logger.debug('# Entering object_id_merge_from_args(args)')
 
-    # Read in each shear tables and add the IDs in it to a global set for each method
-    ids = dict.fromkeys(methods)
+    # Keep a list of all shear estimates tables for each method
+    shear_estimates_tables = dict.fromkeys(methods)
     for method in methods:
-        ids[method] = set()
+        # Start with an empty list of the tables
+        shear_estimates_tables[method] = []
 
-    shear_estimates_table_product_filenames = read_listfile(os.path.join(args.workdir, args.input_shear_estimates_listfile))
+    shear_estimates_table_product_filenames = read_listfile(
+        os.path.join(args.workdir, args.input_shear_estimates_listfile))
+
+    # We'll use a failsafe exception block in case of corrupt data, and combine what we can. But raise
+    # an exception if we can't get anything
+    at_least_one_good_estimates_table = False
 
     for shear_estimates_table_product_filename in shear_estimates_table_product_filenames:
 
-        # Read in the product and get the filename of the table
+        try:
 
-        shear_estimates_table_product = read_xml_product(os.path.join(args.workdir, shear_estimates_table_product_filename))
+            logger.debug("Loading shear estimates from file: " + shear_estimates_table_product_filename)
 
-        if not isinstance(shear_estimates_table_product, shear_estimates.dpdShearMeasurement):
-            raise TypeError("Shear product is of invalid type: ")
+            # Read in the product and get the filename of the table
+
+            shear_estimates_table_product = read_xml_product(
+                os.path.join(args.workdir, shear_estimates_table_product_filename))
+
+            if not isinstance(shear_estimates_table_product, shear_estimates.dpdShearMeasurement):
+                raise TypeError("Shear product is of invalid type: " + type(shear_estimates_table_product))
+
+        except Exception as e:
+
+            logger.warn("Failsafe block encountered exception: " + str(e))
+            continue
 
         # Loop over methods and read in the table
 
         for method in methods:
 
-            shear_estimates_method_table_filename = shear_estimates_table_product.get_method_filename(method)
+            try:
 
-            shear_estimates_method_table = Table.read(os.path.join(args.workdir, shear_estimates_method_table_filename))
+                shear_estimates_method_table_filename = shear_estimates_table_product.get_method_filename(method)
 
-            if not is_in_format(shear_estimates_method_table, setf, verbose=True, ignore_metadata=True):
-                raise TypeError("Input shear estimates table for method {} is of invalid format.".format(method))
+                if shear_estimates_method_table_filename is None or shear_estimates_method_table_filename == "None":
+                    logger.debug("No shear estimates available for method: " + method)
+                    continue
 
-            # Get the ID list from it and add it to the set
-            ids[method].update(shear_estimates_method_table[setf.ID].data)
+                shear_estimates_method_table = Table.read(os.path.join(
+                    args.workdir, shear_estimates_method_table_filename))
 
-    # Get a filename
-    shear_estimates_product_filename = get_allowed_filename(type_name="SHE-EST-LIST",
-                                                            instance_id='merged',
-                                                            extension=".xml",
-                                                            release="00.07",
-                                                            subdir="data",
-                                                            processing_function="SHE")
+                if not is_in_format(shear_estimates_method_table, setf, verbose=True, ignore_metadata=True):
+                    raise TypeError("Input shear estimates table for method {} is of invalid format.".format(method))
 
-    # Create and save the product
-    shear_object_list_product = object_id_list.create_dpd_she_object_id_list(id_list=ids)
-    write_xml_product(shear_object_list_product, os.path.join(args.workdir, shear_estimates_product_filename))
+                # Get the ID list from it and add it to the set
+                shear_estimates_tables[method].append(shear_estimates_method_table)
 
-    logger.debug('# Entering object_id_merge_from_args normally')
+                if len(shear_estimates_method_table) > 0:
+                    at_least_one_good_estimates_table = True
+
+            except Exception as e:
+
+                logger.warn("Failsafe block encountered exception: " + str(e))
+                continue
+
+    if not at_least_one_good_estimates_table:
+        raise RuntimeError("Not able to load any shear estimates tables successfully.")
+
+    # Combine the shear estimates tables for each method and output them
+
+    combined_shear_estimates_tables = dict.fromkeys(methods)
+
+    # Create the output product
+    combined_shear_estimates_product = products.shear_estimates.create_shear_estimates_product()
+
+    for method in methods:
+
+        # Skip if no data for this method
+        if len(shear_estimates_tables[method]) == 0:
+            combined_shear_estimates_product.set_method_filename(method, "None")
+            continue
+
+        # Combine the tables
+        combined_shear_estimates_tables[method] = table.vstack(shear_estimates_tables[method])
+
+        # Get a filename for the table
+        combined_shear_estimates_table_filename = get_allowed_filename(type_name="SHEAR-EST-" + method.upper(),
+                                                                       instance_id='MERGED',
+                                                                       extension=".fits",
+                                                                       release="00.07",
+                                                                       subdir="data",
+                                                                       processing_function="SHE")
+        combined_shear_estimates_product.set_method_filename(method, combined_shear_estimates_table_filename)
+
+        # Output the table
+        combined_shear_estimates_tables[method].write(os.path.join(args.workdir,
+                                                                   combined_shear_estimates_table_filename),
+                                                      format="fits")
+
+    # Get a filename for the output product
+    combined_shear_estimates_product_filename = get_allowed_filename(type_name="SHEAR-EST",
+                                                                     instance_id='MERGED',
+                                                                     extension=".xml",
+                                                                     release="00.07",
+                                                                     subdir="data",
+                                                                     processing_function="SHE")
+
+    # Save the product
+    write_xml_product(combined_shear_estimates_product, os.path.join(args.workdir,
+                                                                     combined_shear_estimates_product_filename))
+
+    logger.debug('# Exiting object_id_merge_from_args normally')
 
     return
 
