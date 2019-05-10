b7e605b3c4de056b3e035896d00b1837a0b0f559
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-05-10 16:37:05 +0100

    Need to import vstack

--- SHE_CTE_ShearValidation/python/SHE_CTE_ShearValidation/match_to_tu.py
+++ SHE_CTE_ShearValidation/python/SHE_CTE_ShearValidation/match_to_tu.py
@@ -31,7 +31,7 @@ from SHE_PPT.table_utility import table_to_hdu
 from astropy import units
 from astropy.coordinates import SkyCoord
 from astropy.io import fits
-from astropy.table import Table, Column, join
+from astropy.table import Table, Column, join, vstack
 import numpy as np
 
 
