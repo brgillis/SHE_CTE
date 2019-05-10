da1cc96006e78c62c242f17ebcc4acdd9e22dd05
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-05-10 14:01:53 +0100

    Adding skeleton of match_to_tu_from_args function

---
+++ SHE_CTE_ShearValidation/python/SHE_CTE_ShearValidation/match_to_tu.py
@@ -0,0 +1,30 @@
+""" @file match_to_tu.py
+
+    Created 10 May 2019
+
+    Code to implement matching of shear estimates catalogs to SIM's TU galaxy and star catalogs.
+"""
+
+__updated__ = "2019-05-10"
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from SHE_PPT.logging import getLogger
+
+
+def match_to_tu_from_args(args):
+    """ @TODO Fill in docstring
+    """
+
+    pass
