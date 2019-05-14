82d7825b802742744a91a5fae507b803eda046d1
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-05-14 14:34:05 +0100

    Moving correction function to SHE_PPT

--- SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/galsim_estimate_shear.py
+++ SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/galsim_estimate_shear.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-__updated__ = "2019-05-03"
+__updated__ = "2019-05-14"
 
 from copy import deepcopy
 from math import sqrt
@@ -31,7 +31,7 @@ from SHE_PPT import mdb
 from SHE_PPT.logging import getLogger
 from SHE_PPT.magic_values import scale_label, gain_label
 from SHE_PPT.she_image import SHEImage
-from SHE_PPT.shear_utility import get_g_from_e
+from SHE_PPT.shear_utility import get_g_from_e, correct_for_wcs_shear_and_rotation
 from SHE_PPT.table_formats.detections import tf as detf
 from SHE_PPT.table_formats.shear_estimates import initialise_shear_estimates_table, tf as setf
 from SHE_PPT.utility import run_only_once
@@ -222,99 +222,6 @@ def get_REGAUSS_shear_estimate(galsim_shear_estimate, scale):
     logger.debug("Exiting get_REGAUSS_shear_estimate")
 
     return shear_estimate
-
-
-def correct_for_wcs_shear_and_rotation(shear_estimate, stamp):
-    """ Corrects (in-place) a shear_estimate object for the shear and rotation information contained within the
-        provided stamp's wcs. 
-    """
-
-    # Since we have to solve for the pre-wcs shear, we get the world2pix decomposition and work backwards
-    _scale, w2p_shear, w2p_theta, _w2p_flip = stamp.get_world2pix_decomposition()
-
-    # Set up the shear as a matrix
-    g_pix_polar = np.matrix([[shear_estimate.g1], [shear_estimate.g2]])
-
-    # We first have to rotate into the proper frame
-    sintheta = w2p_theta.sin()
-    costheta = w2p_theta.cos()
-
-    # Calculate the rotation matrix directly for testing purposes
-    test_p2w_rotation_matrix = stamp.get_pix2world_rotation(shear_estimate.x, shear_estimate.y)
-
-    # Get the reverse rotation matrix
-    p2w_rotation_matrix = np.matrix([[costheta, sintheta], [-sintheta, costheta]])
-
-    double_p2w_rotation_matrix = p2w_rotation_matrix @ p2w_rotation_matrix  # 2x2 so it's commutative
-    g_world_polar = double_p2w_rotation_matrix @ g_pix_polar
-
-    # TODO: Update errors from the WCS shear
-
-    # Update errors from the WCS rotation
-    covar_pix = np.matrix([[shear_estimate.g1_err**2, shear_estimate.g1g2_covar],
-                           [shear_estimate.g1g2_covar, shear_estimate.g2_err**2]])
-    covar_world = double_p2w_rotation_matrix @ covar_pix @ double_p2w_rotation_matrix.transpose()
-
-    # Update error and covar values in the shear_estimate object
-    shear_estimate.g1_err = np.sqrt(covar_world[0, 0])
-    shear_estimate.g2_err = np.sqrt(covar_world[1, 1])
-    shear_estimate.g1g2_covar = covar_world[0, 1]
-
-    # Second, we have to correct for the shear. It's necessary to do this by solving for the pre-WCS shear
-
-    try:
-
-        rot_est_shear = galsim.Shear(g1=g_world_polar[0, 0], g2=g_world_polar[1, 0])
-
-    except ValueError as e:
-
-        if not "Requested shear exceeds 1" in str(e):
-            raise
-
-        # Shear is greater than 1, so note this in the flags
-        shear_estimate.g1 = np.NaN
-        shear_estimate.g2 = np.NaN
-        shear_estimate.gerr = np.inf
-        shear_estimate.g1_err = np.inf
-        shear_estimate.g2_err = np.inf
-        shear_estimate.g1g2_covar = np.inf
-
-        shear_estimate.flags |= flags.flag_too_large_shear
-
-        return
-
-    def get_shear_adding_diff(g):
-        g1 = g[0]
-        g2 = g[1]
-        try:
-            res_shear = w2p_shear + galsim.Shear(g1=g1, g2=g2)
-            dist2 = (rot_est_shear.g1 - res_shear.g1)**2 + (rot_est_shear.g2 - res_shear.g2)**2
-        except ValueError as e:
-            if not "Requested shear exceeds 1" in str(e):
-                raise
-            # Requested a too-high shear value, so return an appropriately high distance
-            dist2 = (w2p_shear.g1 + g1 - rot_est_shear.g1)**2 + (w2p_shear.g2 + g2 - rot_est_shear.g2)**2
-        return dist2
-
-    fitting_result = minimize(get_shear_adding_diff, np.array((0, 0)))
-
-    # If we can't find a solution, return NaN shear
-    if not fitting_result.success:
-        shear_estimate.g1 = np.NaN
-        shear_estimate.g2 = np.NaN
-        shear_estimate.gerr = np.inf
-        shear_estimate.g1_err = np.inf
-        shear_estimate.g2_err = np.inf
-        shear_estimate.g1g2_covar = np.inf
-
-        shear_estimate.flags |= flags.flag_cannot_correct_distortion
-
-        return
-    else:
-        shear_estimate.g1 = fitting_result.x[0]
-        shear_estimate.g2 = fitting_result.x[1]
-
-    return
 
 
 def check_data_quality(gal_stamp, psf_stamp, stacked=False):
