49e74aaf3f860dbf86bc129113998c2ec188fc9b
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-08-27 17:13:17 +0200

    Incrementing to version 0.8.12 and updating dependencies

--- SHE_CTE/python/SHE_CTE/__init__.py
+++ SHE_CTE/python/SHE_CTE/__init__.py
@@ -5,7 +5,7 @@
     SHE_CTE package, for modules general to SHE_CTE
 """
 
-__updated__ = "2019-08-16"
+__updated__ = "2019-08-27"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -20,4 +20,4 @@ __updated__ = "2019-08-16"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-__version__ = "0.8.11"
+__version__ = "0.8.12"
