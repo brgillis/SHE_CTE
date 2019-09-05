38af0215da4902a20c5469710e2b445315504a33
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-09-05 12:11:40 +0100

    Update to version 0.8.13

--- SHE_CTE/python/SHE_CTE/__init__.py
+++ SHE_CTE/python/SHE_CTE/__init__.py
@@ -20,4 +20,4 @@ __updated__ = "2019-08-27"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-__version__ = "0.8.12"
+__version__ = "0.8.13"
