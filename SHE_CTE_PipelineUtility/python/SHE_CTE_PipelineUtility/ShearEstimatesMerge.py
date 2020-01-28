c6f124284e940f6211ec692890e50283e1c5bbea
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2020-01-28 17:00:14 +0000

    Fixing spatial footprint setting for merge

--- SHE_CTE_PipelineUtility/python/SHE_CTE_PipelineUtility/ShearEstimatesMerge.py
+++ SHE_CTE_PipelineUtility/python/SHE_CTE_PipelineUtility/ShearEstimatesMerge.py
@@ -221,7 +221,7 @@ def shear_estimates_merge_from_args(args):
 
     # Create the output product
     combined_shear_estimates_product = products.shear_estimates.create_shear_estimates_product(
-        spatial_footprint=shear_estimates_table_product_filenames)
+        spatial_footprint=shear_estimates_table_product_filenames[0])
 
     for method in methods:
 
