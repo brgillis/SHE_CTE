2de4b60e3b817248a70f2423801fdf4441a3bfd1
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2020-08-13 18:40:34 +0200

    Filling in code for combining shear estimates

--- SHE_CTE_ShearReconciliation/python/SHE_CTE_ShearReconciliation/reconciliation_functions.py
+++ SHE_CTE_ShearReconciliation/python/SHE_CTE_ShearReconciliation/reconciliation_functions.py
@@ -5,7 +5,7 @@
     Functions to handle different ways of reconciling different shear estimates.
 """
 
-__updated__ = "2020-08-04"
+__updated__ = "2020-08-13"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -20,8 +20,10 @@ __updated__ = "2020-08-04"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-import numpy as np
 from SHE_PPT.utility import run_only_once
+
+import numpy as np
+
 
 def reconcile_best(measurements_to_reconcile_table,
                    output_row,
@@ -60,25 +62,15 @@ def reconcile_best(measurements_to_reconcile_table,
 
     return
 
-# Data used for weight reconciliation
-props_with_independent_errors = ("ra", "dec", "re", "flux", "bulge_frac", "snr", "sersic")
-props_to_sum = ("nexp", "weight")
-props_to_bitwise_or = ("fit_flags", "val_flags", )
-props_to_copy = ("ID", "fit_class")
-props_to_nan = ("chi2", "dof", "g1g2_covar", "g1g2_uncal_covar")
 
-@run_only_once
-def warn_missing_props(missing_props):
-    warning_string = "The following properties have no defined method to combine them in the reconcile_weight function:"
-    for prop in missing_props:
-        warning_string += " " + prop + ","
-    logger.warning(warning_string[:-1])
-
-def reconcile_weight(measurements_to_reconcile_table,
-                     output_row,
-                     sem_tf,
-                     *args, **kwargs):
-    """ Reconciliation method which combines measurements based on their weights.
+def reconcile_shape_weight(measurements_to_reconcile_table,
+                           output_row,
+                           sem_tf,
+                           *args, **kwargs):
+    """ Reconciliation method which combines measurements based on supplied shape
+        measurement weights.
+    
+    
         Parameters
         ----------
         measurements_to_reconcile_table : astropy.table.Table
@@ -100,34 +92,120 @@ def reconcile_weight(measurements_to_reconcile_table,
         None
     """
 
-    # Determine the weight for each row
-    if nexp in vars(sem_tf):
-        weights = measurements_to_reconcile_table[sem_tf.weight] * measurements_to_reconcile_table[sem_tf.nexp]
-    else:
-        weights = measurements_to_reconcile_table[sem_tf.weight]
+    weights = measurements_to_reconcile_table[sem_tf.weight]
+
+    return reconcile_weight(measurements_to_reconcile_table=measurements_to_reconcile_table,
+                            output_row=output_row,
+                            sem_tf=sem_tf,
+                            weights=weights,
+                            *args, **kwargs)
+
+
+def reconcile_invvar(measurements_to_reconcile_table,
+                     output_row,
+                     sem_tf,
+                     *args, **kwargs):
+    """ Reconciliation method which combines measurements based on inverse-shape-variance
+        weighting.
     
+    
+        Parameters
+        ----------
+        measurements_to_reconcile_table : astropy.table.Table
+            The table containing rows of different measurements of the same object
+        output_row : row of astropy.table.Row
+            The row to which to output the reconciled measurement
+        sem_tf : Table Format
+            The table format of the measurements table
+        *args, **kwards
+            Needed in case a different reconciliation method has an expanded interface
+            
+        Side-effects
+        ------------
+        - output_row is updated with the reconciled measurement
+        - measurements_to_reconcile_table is sorted by weight
+    
+        Return
+        ------
+        None
+    """
+
+    weights = 1 / (0.5 * (measurements_to_reconcile_table[sem_tf.g1_err] ** 2 +
+                        measurements_to_reconcile_table[sem_tf.g2_err] ** 2))
+
+    return reconcile_weight(measurements_to_reconcile_table=measurements_to_reconcile_table,
+                            output_row=output_row,
+                            sem_tf=sem_tf,
+                            weights=weights,
+                            *args, **kwargs)
+
+
+# Data used for weight reconciliation
+props_with_independent_errors = ("g1", "g2", "g1_uncal", "g2_uncal", "ra", "dec", "re", "flux",
+                                 "bulge_frac", "snr", "sersic")
+props_to_sum = ("nexp",)
+props_to_bitwise_or = ("fit_flags", "val_flags",)
+props_to_copy = ("ID", "fit_class")
+props_to_nan = ("chi2", "dof", "g1g2_covar", "g1g2_uncal_covar")
+
+
+@run_only_once
+def warn_missing_props(missing_props):
+    warning_string = "The following properties have no defined method to combine them in the reconcile_weight function:"
+    for prop in missing_props:
+        warning_string += " " + prop + ","
+    logger.warning(warning_string[:-1])
+
+
+def reconcile_weight(measurements_to_reconcile_table,
+                     output_row,
+                     sem_tf,
+                     weights,
+                     *args, **kwargs):
+    """ Reconciliation method which combines measurements based on given weights.
+        Parameters
+        ----------
+        measurements_to_reconcile_table : astropy.table.Table
+            The table containing rows of different measurements of the same object
+        output_row : row of astropy.table.Row
+            The row to which to output the reconciled measurement
+        sem_tf : Table Format
+            The table format of the measurements table
+        weights : Iterable <float>
+            The weights to use for each row of the table
+        *args, **kwards
+            Needed in case a different reconciliation method has an expanded interface
+            
+        Side-effects
+        ------------
+        - output_row is updated with the reconciled measurement
+        - measurements_to_reconcile_table is sorted by weight
+    
+        Return
+        ------
+        None
+    """
+
     # For any instances of NaN, set the weight to zero
-    weights = np.where(np.logical_or(np.isnan(weights),np.isinf(weights)), 0, weights)
+    weights = np.where(np.logical_or(np.isnan(weights), np.isinf(weights)), 0, weights)
     tot_weight = np.sum(weights)
     highest_weight_index = np.argmax(weights)
 
     new_props = {}
-
-    # TODO - implement special handling for shear, which has non-independent errors
 
     # Combine each value we can combine simply (where all errors are independent)
     for prop in props_with_independent_errors:
         if not prop in vars(sem_tf):
             continue
         colname = getattr(sem_tf, prop)
-        new_props[colname] = np.sum(measurements_to_reconcile_table[colname]*weights)/tot_weight
+        new_props[colname] = np.sum(measurements_to_reconcile_table[colname] * weights) / tot_weight
 
         # If this property has an error, calculate that too
         prop_err = colname + "_err"
         if not prop_err in vars(sem_tf):
             continue
         colname_err = getattr(sem_tf, prop_err)
-        new_props[colname_err] = np.sqrt(np.sum(np.pow(measurements_to_reconcile_table[colname_err]*weights,2))/tot_weight)
+        new_props[colname_err] = np.sqrt(np.sum(np.pow(measurements_to_reconcile_table[colname_err] * weights, 2)) / tot_weight)
 
     # Combine properties we sum up
     for prop in props_to_bitwise_or:
@@ -167,6 +245,20 @@ def reconcile_weight(measurements_to_reconcile_table,
             output_row[prop] = np.NaN
         warn_missing_props(missing_props)
 
-    return
+    # Figure out what the shape noise is from the shape errors and weights, and use it to calculate weight
+    inv_weight_column = 1 / measurements_to_reconcile_table[sem_tf.weight]
+    average_shape_var = (0.5 * (measurements_to_reconcile_table[sem_tf.g1_err] ** 2 +
+                         measurements_to_reconcile_table[sem_tf.g2_err] ** 2))
+    if not (inv_weight_column > average_shape_var).all():
+        logger.warn("Cannot determine shape noise for object " + str(measurements_to_reconcile_table[sem_tf.ID]) + " with:"
+                    "weight = " + str(measurements_to_reconcile_table[sem_tf.weight]) +
+                    "\ng1_err = " + str(measurements_to_reconcile_table[sem_tf.g1_err]) +
+                    "\ng2_err = " + str(measurements_to_reconcile_table[sem_tf.g2_err]) +
+                    "\nSetting weight to 0.")
+        new_props[sem_tf.weight] = 0.
+    else:
+        shape_var = inv_weight_column - average_shape_var
+        new_props[sem_tf.weight] = 1. / (0.5 * (new_props[sem_tf.g1_err] ** 2 + new_props[sem_tf.g2_err] ** 2) + shape_var)
 
+    return
 
