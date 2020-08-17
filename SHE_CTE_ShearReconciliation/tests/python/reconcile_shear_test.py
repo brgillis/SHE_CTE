5edbf5008a1bb48104eaef4b21c0676fc0b9cdd1
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2020-08-17 18:55:27 +0100

    Adding template unit test

---
+++ SHE_CTE_ShearReconciliation/tests/python/reconcile_shear_test.py
@@ -0,0 +1,41 @@
+""" @file reconcile_shear_test.py
+
+    Created 17 Aug 2020
+
+    Unit tests for the shear reconciliation task.
+"""
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import pytest
+
+import numpy as np
+
+
+class TestCase:
+    """
+    """
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        pass
+
+    def test_reconcile_best(self):
+        pass
+
+    def test_reconcile_shape_weight(self):
+        pass
+
+    def test_reconcile_invvar(self):
+        pass
