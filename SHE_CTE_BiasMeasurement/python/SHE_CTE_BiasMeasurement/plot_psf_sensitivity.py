1488a46dde4d67e0b308af35fa5e5e9e2dd6a3a6
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2020-09-15 16:45:35 +0200

    Fixing bug with wrong calibration label

--- SHE_CTE_BiasMeasurement/python/SHE_CTE_BiasMeasurement/plot_psf_sensitivity.py
+++ SHE_CTE_BiasMeasurement/python/SHE_CTE_BiasMeasurement/plot_psf_sensitivity.py
@@ -292,7 +292,7 @@ def plot_psf_sensitivity_from_args(args):
                         y1_errs = y1_o_vals
                         y2_errs = y2_o_vals
                         # Carry over errors from previous run, on unnormed data
-                        y_errs = all_methods_data[(method, calibration_label)]["y_err"]
+                        y_errs = all_methods_data[(method, "")]["y_err"]
                     else:
                         y_vals = np.sqrt(y1_vals**2 + y2_vals**2)
                         y1_errs = y1_o_vals
