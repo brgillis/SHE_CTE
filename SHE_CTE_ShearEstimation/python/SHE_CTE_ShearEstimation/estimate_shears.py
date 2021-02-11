8c3041c95c8e29a5f48805f8e87e4c5b921361bc
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-02-11 15:16:30 +0100

    Getting table_to_hdu from proper astropy location

--- SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/estimate_shears.py
+++ SHE_CTE_ShearEstimation/python/SHE_CTE_ShearEstimation/estimate_shears.py
@@ -5,7 +5,7 @@
     Primary execution loop for measuring galaxy shapes from an image file.
 """
 
-__updated__ = "2021-01-25"
+__updated__ = "2021-02-11"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -41,9 +41,10 @@ from SHE_PPT.table_formats.she_lensmc_measurements import initialise_lensmc_meas
 from SHE_PPT.table_formats.she_measurements import tf as sm_tf
 from SHE_PPT.table_formats.she_momentsml_measurements import initialise_momentsml_measurements_table, tf as mmlm_tf
 from SHE_PPT.table_formats.she_regauss_measurements import initialise_regauss_measurements_table, tf as regm_tf
-from SHE_PPT.table_utility import is_in_format, table_to_hdu
+from SHE_PPT.table_utility import is_in_format
 from SHE_PPT.utility import hash_any
 from astropy.io import fits
+from astropy.io.fits import table_to_hdu
 
 import SHE_CTE
 from SHE_CTE_ShearEstimation.control_training_data import load_control_training_data
