a6dcde92faee1e092a6cb7f88d8d9320a6c1fff7
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-04-22 11:53:27 +0100

    Incrementing to v8.1.2

--- SHE_CTE/python/SHE_CTE/__init__.py
+++ SHE_CTE/python/SHE_CTE/__init__.py
@@ -20,7 +20,7 @@ __updated__ = "2021-01-05"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-__version__ = "8.1.1"
+__version__ = "8.1.2"
 
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
