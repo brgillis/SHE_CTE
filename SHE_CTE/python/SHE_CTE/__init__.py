6ab8b27d5ad2663d9073ffdd0d56cd58a36f7cc6
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-05-13 16:02:37 +0100

    v8.1.8 - depends on SHE_PPT 8.8.2

--- SHE_CTE/python/SHE_CTE/__init__.py
+++ SHE_CTE/python/SHE_CTE/__init__.py
@@ -20,7 +20,7 @@ __updated__ = "2021-05-06"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-__version__ = "8.1.7"
+__version__ = "8.1.8"
 
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
