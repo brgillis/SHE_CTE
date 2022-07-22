2a2b910e6227013c0d65a4aa648b21461b3f478e
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-07-22 14:49:37 +0100

    Cleaning up file header section

--- SHE_CTE_ShearReconciliation/python/SHE_CTE_ShearReconciliation/reconcile_shear.py
+++ SHE_CTE_ShearReconciliation/python/SHE_CTE_ShearReconciliation/reconcile_shear.py
@@ -5,7 +5,7 @@
     Primary execution loop for reconciling shear estimates into a per-tile catalog.
 """
 
-__updated__ = "2021-08-18"
+__updated__ = "2022-08-22"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -43,23 +43,6 @@ from SHE_CTE_ShearReconciliation.chains_reconciliation_functions import (reconci
 from SHE_CTE_ShearReconciliation.reconciliation_functions import (reconcile_best,
                                                                   reconcile_invvar,
                                                                   reconcile_shape_weight)
-import numpy as np
-
-
-__updated__ = "2021-03-10"
-
-# Copyright (C) 2012-2020 Euclid Science Ground Segment
-#
-# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
-# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
-# any later version.
-#
-# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 
 logger = getLogger(__name__)
