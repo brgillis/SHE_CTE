442a682c9ddc480bc85c4322f323f358a46ae522
Richard Rollins <689-rrollins@users.noreply.gitlab.euclid-sgs.uk>
2023-04-04 11:10:17 +0000

    Use pathlib API

--- SHE_CTE_PipelineUtility/tests/python/converttohdf5_test.py
+++ SHE_CTE_PipelineUtility/tests/python/converttohdf5_test.py
@@ -44,13 +44,12 @@ OBJSIZE = 2.5
 @pytest.fixture
 def workdir(tmp_path):
     """Creates a workdir (via pytest's tmpdir fixture) and a datadir"""
-    workdir = tmp_path
 
     # make the datadir
-    datadir = os.path.join(workdir, "data")
-    os.mkdir(datadir)
+    datadir = tmp_path / "data"
+    datadir.mkdir()
 
-    return workdir
+    return tmp_path
 
 
 @pytest.fixture
